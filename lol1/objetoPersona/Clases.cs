0c43868403f93f88dfbd87be6ba82a6d432fb67d
santuTerri <130667404+santuTerri@users.noreply.github.com>
2024-08-06 21:02:40 -0300

    Add files via upload

---
+++ lol1/objetoPersona/Clases.cs
@@ -0,0 +1,65 @@
+namespace libreriaClase{
+
+    class Persona
+    {
+
+//      private string nombre;
+
+
+        public string Apellido {get;set;}
+        public string Nombre {get;set;}
+        public int DNI {get;set;}
+        public string FechaNacimiento {get;set;}
+
+
+       
+
+        public  Persona(string rApellido,string rNombre, string rFechaNacimiento, int rDni){
+            Apellido = rApellido;
+            Nombre = rNombre;
+            FechaNacimiento = rFechaNacimiento;
+            DNI = rDni;
+
+        }
+
+        public Persona(){
+            
+        }
+        public string caminar(){
+
+            return "Caminando....";
+
+        }
+
+        public void mostrarPersona(){
+            Console.WriteLine("Apellido: {0}", Apellido);
+            Console.WriteLine("Nombre: {0}", Nombre);
+            Console.WriteLine("Fecha Nacimiento: {0}", FechaNacimiento);
+            Console.WriteLine("DNI: {0}", DNI);
+        }
+
+        public void devolverEdad(){
+            Console.WriteLine("La edad es:.....");
+        }
+
+    }
+
+
+    class Alumno : Persona {
+
+        private int legajo;
+        private string curso;
+
+        private int[] notas;
+
+
+        public void estudiar(){
+            Console.WriteLine("Estudiando....");
+        }
+
+        public void devolverCurso(){
+            Console.WriteLine("El curso es: {0}", this.curso);
+        }
+    }
+
+}
---
+++ lol1/objetoPersona/Principal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Security.AccessControl;
+using libreriaClase;
+
+namespace libreria
+{
+
+    class Pricipal{
+        static void Main(){
+
+            string opcion = mostrarMenu();
+
+            while ( opcion != "9") {
+
+            if (opcion == "1"){
+
+                crearPersona();
+            } 
+            else
+            {
+                Console.WriteLine("Opción Incorrecta...");
+                Console.ReadKey();
+            }
+
+            opcion = mostrarMenu();
+
+            }
+
+        }
+
+         public static void crearPersona(){
+
+            Console.Clear();
+            Persona p1 = new Persona();
+
+            Console.WriteLine("Ingreso de Personas");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("");
+
+            Console.Write("Ingrese Apellido: ");
+            p1.Apellido = Console.ReadLine();
+
+            Console.Write("Ingrese Nombre: ");
+            p1.Nombre = Console.ReadLine();
+
+            Console.Write("Ingrese DNI: ");
+            p1.DNI = Convert.ToInt32(Console.ReadLine());            
+            
+
+            Console.Write("Ingrese Fecha Nacimiento: ");
+            p1.FechaNacimiento = Console.ReadLine();
+
+        }
+
+
+        public static string mostrarMenu(){
+
+            string opcion;
+
+            Console.Clear();
+
+            Console.WriteLine("Menú");
+            Console.WriteLine("-----");
+
+            Console.WriteLine("");
+            Console.WriteLine("1.- Crear Alumno");
+            Console.WriteLine("");
+            Console.Write("Elija una opción: ");
+
+            opcion = Console.ReadLine();
+
+            return opcion;
+
+
+        }
+    }
+
+
+    
+}
